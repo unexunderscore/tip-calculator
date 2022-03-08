2d8e2ca368df4828a4639e71d522adeb50e3b225
Attila Tóth <toth_attila@outlook.hu>
2022-03-08 17:04:40 +0100

    Add Bill select, add input that only accept numbers (or dot sign).

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const greetings = "Hi there!";
 console.log(greetings);
+
+
+// Only support number key press and dot . for decimals
+function validate(evt) {
+    var theEvent = evt || window.event;
+  
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+    // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+  }
