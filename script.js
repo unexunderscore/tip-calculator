9bcd3945a53396a23ecd22c3b00a8004b55b9e92
Attila Tóth <toth_attila@outlook.hu>
2022-03-08 19:47:22 +0100

    Custom option changed to input (before it's was simple div).

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(greetings);
 // Only support number key press and dot . for decimals
 function validate(evt) {
     var theEvent = evt || window.event;
-  
+    
     // Handle paste
     if (theEvent.type === 'paste') {
         key = event.clipboardData.getData('text/plain');
@@ -15,6 +15,26 @@ function validate(evt) {
         key = String.fromCharCode(key);
     }
     var regex = /[0-9]|\./;
+    if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+  }
+
+
+// Only support number key press 
+  function validate_people(evt) {
+    var theEvent = evt || window.event;
+  
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+    // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]/;
     if( !regex.test(key) ) {
       theEvent.returnValue = false;
       if(theEvent.preventDefault) theEvent.preventDefault();
